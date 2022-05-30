592e2e48578e67a1d54ec62e61c2e85ae1427de7
Nidhi Bharani <fishypiscian@gmail.com>
2022-05-31 02:52:48 +0530

    increase batch size

--- recipes/ljspeech/tacotron2-DCA/train_tacotron_dca.py
+++ recipes/ljspeech/tacotron2-DCA/train_tacotron_dca.py
@@ -40,11 +40,11 @@ audio_config = BaseAudioConfig(
 
 config = Tacotron2Config(  # This is the config that is saved for the future use
     audio=audio_config,
-    batch_size=128,
+    batch_size=256,
     lr = 0.05,
-    eval_batch_size=64,
-    num_loader_workers=4, #warning from colab run that appropriate num_workers for that system is 2. If set to 4, training might slow or halt.
-    num_eval_loader_workers=4,
+    eval_batch_size=256,
+    num_loader_workers=16, #warning from colab run that appropriate num_workers for that system is 2. If set to 4, training might slow or halt.
+    num_eval_loader_workers=16,
     run_eval=True,
     test_delay_epochs=-1,
     ga_alpha=0.0,
