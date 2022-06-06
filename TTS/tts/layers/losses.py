eeeaef97346cfbac0d965cbbca68aa40e429fa29
Nidhi Bharani <fishypiscian@gmail.com>
2022-06-06 12:14:56 +0530

    Debugging RuntimeError while reducing r from 7 to 6

--- TTS/tts/layers/losses.py
+++ TTS/tts/layers/losses.py
@@ -160,6 +160,7 @@ class BCELossMasked(nn.Module):
         if length is not None:
             mask = sequence_mask(sequence_length=length, max_len=target.size(1)).float()
             #debugging change in r. RuntimeError: The size of tensor a (18) must match the size of tensor b (21) at non-singleton dimension 1
+            print("debugging r")
             print("mask shape", mask.size())
             print("x shape",x.size())
             x = x * mask
